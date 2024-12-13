2aa8581be1c890e27ccf128adaec63444a190687
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2024-12-13 14:53:21 -0800

    Added functionality to st.cmd by loading drivers and database file

--- iocs/nikolaIOC/iocBoot/iocnikolaIocExample/st.cmd
+++ iocs/nikolaIOC/iocBoot/iocnikolaIocExample/st.cmd
@@ -5,14 +5,25 @@
 
 < envPaths
 
+epicsEnvSet( "DEVICE_PORT",  "127.0.0.1" )
+epicsEnvSet( "LOCA", "OUTS" )
+epicsEnvSet( "POS", "123" )
+epicsEnvSet( "PV_BASE", "TEMP:$(LOCA):$(POS)" )
+epicsEnvSet( "STREAM_PROTOCOL_PATH", "$(TOP)/db" )
+
 cd "${TOP}"
 
 ## Register all support components
 dbLoadDatabase "dbd/nikolaIOC.dbd"
 nikolaIOC_registerRecordDeviceDriver pdbbase
 
+drvAsynIPPortConfigure( "P0", "$(DEVICE_PORT):22222", 0, 0, 0 )
+rateLimitInterposeInit("P0", 500)
+#asynSetTraceMask("P0", 0, "0x08")
+#asynSetTraceIOMask("P0", 0, "0x01")
+
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=marcio")
+dbLoadRecords("db/nikola.db","device=$(PV_BASE),port=P0")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
