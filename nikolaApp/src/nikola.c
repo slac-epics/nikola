67039d238bfa67bc960275547de882bc3934cc80
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2024-12-13 19:23:31 -0800

    The interpose interface was implemented wrongly. All asynOctet functions must be implemented and they need to call the previous interface in order to not break the interface chain.

--- nikolaApp/src/nikola.c
+++ nikolaApp/src/nikola.c
@@ -25,7 +25,7 @@ static asynStatus writeIt(void* ppvt, asynUser* pasynUser,
     const char* data, size_t numchars, size_t* nbytesTransfered);
 static asynStatus readIt(void* ppvt, asynUser* pasynUser,
     char* data, size_t maxchars, size_t* nbytesTransfered, int* eomReason);
-/*static asynStatus flushIt(void *ppvt,asynUser *pasynUser);
+static asynStatus flushIt(void *ppvt,asynUser *pasynUser);
 static asynStatus registerInterruptUser(void *ppvt,asynUser *pasynUser,
     interruptCallbackOctet callback, void *userPvt,void **registrarPvt);
 static asynStatus cancelInterruptUser(void *ppvt,asynUser *pasynUser,
@@ -44,14 +44,84 @@ static asynOctet octet = {
     registerInterruptUser,cancelInterruptUser,
     setInputEos,getInputEos,setOutputEos,getOutputEos
 };
-*/
-static asynOctet octet = {
-    writeIt, readIt, NULL,
-    NULL, NULL, NULL, NULL, NULL, NULL
-};
-
 
 /* asynOctet methods */
+static asynStatus flushIt(void *ppvt, asynUser *pasynUser)
+{
+    rateLimitPvt* pRateLimit = (rateLimitPvt*)ppvt;
+
+    asynPrint(pasynUser,ASYN_TRACEIO_FILTER,
+        "entered rateLimitInterpose::flush\n");
+    return pRateLimit->pasynOctet->flush(
+        pRateLimit->asynOctetPvt, pasynUser);
+}
+
+static asynStatus registerInterruptUser(void *ppvt, asynUser *pasynUser,
+    interruptCallbackOctet callback, void *userPvt, void **registrarPvt)
+{
+    rateLimitPvt* pRateLimit = (rateLimitPvt*)ppvt;
+
+    asynPrint(pasynUser, ASYN_TRACEIO_FILTER,
+        "entered rateLimitInterpose::registerInterruptUser\n");
+    return pRateLimit->pasynOctet->registerInterruptUser(
+        pRateLimit->asynOctetPvt, pasynUser, callback, userPvt, registrarPvt);
+}
+
+static asynStatus cancelInterruptUser(void* ppvt, asynUser* pasynUser,
+    void* registrarPvt)
+{
+    rateLimitPvt* pRateLimit = (rateLimitPvt*)ppvt;
+
+    asynPrint(pasynUser, ASYN_TRACEIO_FILTER,
+        "entered rateLimitInterpose::cancelInterruptUser\n");
+    return pRateLimit->pasynOctet->cancelInterruptUser(
+        pRateLimit->asynOctetPvt, pasynUser, registrarPvt);
+}
+
+static asynStatus setInputEos(void* ppvt, asynUser* pasynUser,
+    const char* eos, int eoslen)
+{
+    rateLimitPvt* pRateLimit = (rateLimitPvt*)ppvt;
+
+    asynPrint(pasynUser, ASYN_TRACEIO_FILTER,
+        "entered rateLimitInterpose::setInputEos\n");
+    return pRateLimit->pasynOctet->setInputEos(pRateLimit->asynOctetPvt,
+        pasynUser, eos, eoslen);
+}
+
+static asynStatus getInputEos(void* ppvt, asynUser* pasynUser,
+    char* eos, int eossize, int* eoslen)
+{
+    rateLimitPvt* pRateLimit = (rateLimitPvt*)ppvt;
+
+    asynPrint(pasynUser, ASYN_TRACEIO_FILTER,
+        "entered rateLimitInterpose::getInputEos\n");
+    return pRateLimit->pasynOctet->getInputEos(pRateLimit->asynOctetPvt,
+        pasynUser, eos, eossize, eoslen);
+}
+
+static asynStatus setOutputEos(void* ppvt,asynUser* pasynUser,
+    const char* eos, int eoslen)
+{
+    rateLimitPvt* pRateLimit = (rateLimitPvt*)ppvt;
+
+    asynPrint(pasynUser, ASYN_TRACEIO_FILTER,
+        "entered rateLimitInterpose::setOutputEos\n");
+    return pRateLimit->pasynOctet->setOutputEos(pRateLimit->asynOctetPvt,
+        pasynUser, eos, eoslen);
+}
+
+static asynStatus getOutputEos(void* ppvt, asynUser* pasynUser,
+    char* eos, int eossize, int* eoslen)
+{
+    rateLimitPvt* pRateLimit = (rateLimitPvt*)ppvt;
+
+    asynPrint(pasynUser, ASYN_TRACEIO_FILTER,
+        "entered rateLimitInterpose::getOutputEos\n");
+    return pRateLimit->pasynOctet->getOutputEos(pRateLimit->asynOctetPvt,
+        pasynUser, eos, eossize, eoslen);
+}
+
 static asynStatus writeIt(void* ppvt, asynUser* pasynUser,
     const char* data, size_t numchars, size_t* nbytesTransfered)
 {
@@ -72,7 +142,8 @@ static asynStatus writeIt(void* ppvt, asynUser* pasynUser,
 
     epicsTimeGetCurrent(&pRateLimit->lastTime);
 
-    return asynSuccess;
+    return pRateLimit->pasynOctet->write(pRateLimit->asynOctetPvt,
+                    pasynUser, data, numchars, nbytesTransfered);
 }
 
 static asynStatus readIt(void* ppvt, asynUser* pasynUser,
@@ -86,7 +157,7 @@ static asynStatus readIt(void* ppvt, asynUser* pasynUser,
     asynPrint(pasynUser, ASYN_TRACEIO_FILTER,
         "entered rateLimitInterpose::read\n");
 
-    epicsTimeGetCurrent(&currTime);
+    /*epicsTimeGetCurrent(&currTime);
     elapsed = epicsTimeDiffInSeconds(&currTime, &pRateLimit->lastTime);
 
     if (elapsed < pRateLimit->minInterval) {
@@ -94,11 +165,12 @@ static asynStatus readIt(void* ppvt, asynUser* pasynUser,
     }
 
     epicsTimeGetCurrent(&pRateLimit->lastTime);
-
-    return asynSuccess;
+*/
+    return pRateLimit->pasynOctet->read(pRateLimit->asynOctetPvt,
+                    pasynUser, data, maxchars, nbytesTransfered, eomReason);;
 }
 
-static int rateLimitInterposeInit(const char* port, int interval)
+static int rateLimitInterposeInit(const char* port, double interval)
 {
     rateLimitPvt* pRateLimit;
     //char *interposeName;
@@ -140,14 +212,14 @@ static int rateLimitInterposeInit(const char* port, int interval)
 static const iocshArg rateLimitInterposeInitArg0 =
     { "portName", iocshArgString };
 static const iocshArg rateLimitInterposeInitArg1 =
-    { "minimumInterval", iocshArgInt };
+    { "minimumInterval", iocshArgDouble };
 static const iocshArg* rateLimitInterposeInitArgs[] =
     {&rateLimitInterposeInitArg0, &rateLimitInterposeInitArg1};
 static const iocshFuncDef rateLimitInterposeInitFuncDef =
     {"rateLimitInterposeInit", 2, rateLimitInterposeInitArgs};
 static void rateLimitInterposeInitCallFunc(const iocshArgBuf* args)
 {
-    rateLimitInterposeInit(args[0].sval, args[1].ival);
+    rateLimitInterposeInit(args[0].sval, args[1].dval);
 }
 
 static void rateLimitInterposeRegister(void)
